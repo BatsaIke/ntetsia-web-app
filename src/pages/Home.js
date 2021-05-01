5701714e749f9c344b78bf3278eff1718c69bbf3
Felix Nana Yaw Yeboah - Jefferson <gabsco208309@hotmail.com>
2021-05-01 20:03:54 +0000

    updated home feeds fetch

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,38 +1,39 @@
-import { Box } from '@chakra-ui/react';
-import React from 'react';
-import FeedBox from 'components/Cards/FeedBox';
-import Post from 'components/Cards/Post';
-import PostSkeleton from 'components/Cards/PostSkeleton';
+import { Box } from "@chakra-ui/react";
+import React from "react";
+import FeedBox from "components/Cards/FeedBox";
+import Post from "components/Cards/Post";
+import PostSkeleton from "components/Cards/PostSkeleton";
 
-import { useFeeds } from 'hooks/useGlobalHooks';
-import Layout from 'container/Layout';
+import Layout from "container/Layout";
+import InfiniteScroll from "react-infinite-scroll-component";
+import useFeeds from "hooks/useFeeds";
 
 const Home = () => {
-  const { feeds, isLoading } = useFeeds();
-
-  // const fetchMore = () => {
-  //   return feeds.links.next;
-  // };
-
+  const { feedsData, page, setPage } = useFeeds();
   return (
-    <Layout pageTitle='Home' px={6}>
-      <Box mb={14}>
+    <Layout pageTitle="Home" px={6}>
+      <Box mb={14} overflowY="scroll">
         <Box mb={6}>
           <FeedBox />
         </Box>
 
-        {isLoading ? (
-          <Box minW='100%'>
-            <PostSkeleton />
-            <PostSkeleton />
-            <PostSkeleton />
-            <PostSkeleton />
-          </Box>
-        ) : (
-          feeds?.data?.map((feed, i) => (
+        <InfiniteScroll
+          next={() => setPage(page + 1)}
+          hasMore={true}
+          dataLength={feedsData.length}
+          loader={
+            <Box minW="100%">
+              <PostSkeleton />
+              <PostSkeleton />
+              <PostSkeleton />
+              <PostSkeleton />
+            </Box>
+          }
+        >
+          {feedsData.map((feed) => (
             <Post key={feed.id} feed={feed} user={feed?.member} />
-          ))
-        )}
+          ))}
+        </InfiniteScroll>
       </Box>
     </Layout>
   );

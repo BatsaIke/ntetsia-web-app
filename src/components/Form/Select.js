f81af1969d1dc7fe2c5d2d946b9e8e0f5b95ce42
Felix Nana Yaw Yeboah - Jefferson <gabsco208309@hotmail.com>
2021-05-01 19:57:12 +0000

    updated font family

--- src/components/Form/Select.js
+++ src/components/Form/Select.js
@@ -8,13 +8,13 @@ import {
   Image,
   Text,
   useColorMode,
-} from '@chakra-ui/react';
-import { AnimatePresence, motion } from 'framer-motion';
-import { useOnClickOutside } from 'hooks/useOnClickOutside';
-import { Check, ChevronDown, ChevronUp } from 'theme/Icons';
-import React from 'react';
+} from "@chakra-ui/react";
+import { AnimatePresence, motion } from "framer-motion";
+import { useOnClickOutside } from "hooks/useOnClickOutside";
+import { Check, ChevronDown, ChevronUp } from "theme/Icons";
+import React from "react";
 
-const MotionBox = motion.custom(Box);
+const MotionBox = motion(Box);
 
 const Select = ({
   label,
@@ -28,7 +28,6 @@ const Select = ({
   setFieldError,
   error,
   touch,
-  width = '100%',
 }) => {
   const [onOpen, setOnOpen] = React.useState(false);
   const ref = React.useRef();
@@ -49,46 +48,46 @@ const Select = ({
   };
 
   return (
-    <FormControl pos='relative' isRequired isInvalid={error && touch}>
+    <FormControl pos="relative" isRequired isInvalid={error && touch}>
       <FormLabel>{label}</FormLabel>
-      <Box as='span' d='inline-block' w='100%' rounded='md' shadow='sm'>
+      <Box as="span" d="inline-block" w="100%" rounded="md" shadow="sm">
         <MotionBox
-          as='button'
-          type='button'
-          aria-haspopup='listbox'
-          aria-expanded='true'
-          aria-labelledby='listbox-label'
-          cursor='default'
-          pos='relative'
-          w='100%'
+          as="button"
+          type="button"
+          aria-haspopup="listbox"
+          aria-expanded="true"
+          aria-labelledby="listbox-label"
+          cursor="default"
+          pos="relative"
+          w="100%"
           borderWidth={2}
-          borderColor='gray.400'
+          borderColor="gray.400"
           pl={3}
           pr={10}
           py={2}
           h={12}
-          _hover={{ borderColor: 'brand.darkBlue' }}
-          _focus={{ borderColor: 'brand.darkBlue', outline: 'none' }}
-          textAlign='left'
+          _hover={{ borderColor: "brand.darkBlue" }}
+          _focus={{ borderColor: "brand.darkBlue", outline: "none" }}
+          textAlign="left"
           onClick={handleClickBtn}
         >
-          <Flex align='center' ml={3}>
+          <Flex align="center" ml={3}>
             {selected && selected?.flag && (
               <Image src={selected && selected?.flag} w={4} h={4} />
             )}
             <Text
-              as='span'
-              fontSize='md'
-              fontFamily='medium'
+              as="span"
+              fontSize="md"
+              fontFamily="body"
               fontWeight={500}
-              d='block'
+              d="block"
               isTruncated
               color={
                 selected
-                  ? colorMode === 'dark'
-                    ? 'white'
-                    : 'gray.800'
-                  : 'gray.400'
+                  ? colorMode === "dark"
+                    ? "white"
+                    : "gray.800"
+                  : "gray.400"
               }
               ml={3}
             >
@@ -96,16 +95,16 @@ const Select = ({
             </Text>
           </Flex>
           <Flex
-            as='span'
-            pos='absolute'
+            as="span"
+            pos="absolute"
             top={0}
             bottom={0}
             right={0}
-            align='center'
+            align="center"
             pr={2}
-            pointerEvents='none'
-            transitionDuration={onOpen && '250ms'}
-            color='gray.400'
+            pointerEvents="none"
+            transitionDuration={onOpen && "250ms"}
+            color="gray.400"
           >
             <Icon as={onOpen ? ChevronUp : ChevronDown} boxSize={6} />
           </Flex>
@@ -117,64 +116,64 @@ const Select = ({
         {onOpen && (
           <MotionBox
             ref={ref}
-            pos='absolute'
+            pos="absolute"
             mt={1}
-            w='100%'
-            rounded='md'
-            bg='white'
-            shadow='lg'
+            w="100%"
+            rounded="md"
+            bg="white"
+            shadow="lg"
             initial={{ y: -10, opacity: 0 }}
             animate={{ y: 0, opacity: 1, transition: { duration: 0.3 } }}
             exit={{ y: -10, opacity: 0, transition: { duration: 0.3 } }}
             zIndex={10}
           >
             <Box
-              as='ul'
-              tabIndex='-1'
-              role='listbox'
-              aria-labelledby='listbox-label'
-              aria-activedescendant='listbox-item-3'
+              as="ul"
+              tabIndex="-1"
+              role="listbox"
+              aria-labelledby="listbox-label"
+              aria-activedescendant="listbox-item-3"
               maxH={64}
-              rounded='md'
-              fontSize={{ base: 'sm', md: 'md' }}
+              rounded="md"
+              fontSize={{ base: "sm", md: "md" }}
               lineHeight={{ base: 6, md: 5 }}
-              shadow='xs'
-              overflow='auto'
-              _focus={{ outline: 'none' }}
+              shadow="xs"
+              overflow="auto"
+              _focus={{ outline: "none" }}
             >
               {items.map((item) => (
                 <Box
                   key={item.alpha2Code}
-                  as='li'
-                  id='listbox-item-0'
-                  role='option'
-                  color={selected?.name === item.name ? 'white' : 'gray.900'}
-                  cursor='pointer'
-                  userSelect='none'
-                  pos='relative'
+                  as="li"
+                  id="listbox-item-0"
+                  role="option"
+                  color={selected?.name === item.name ? "white" : "gray.900"}
+                  cursor="pointer"
+                  userSelect="none"
+                  pos="relative"
                   py={3}
                   pl={3}
                   pr={8}
-                  bg={selected?.name === item.name && 'brand.lightBlue'}
+                  bg={selected?.name === item.name && "brand.lightBlue"}
                   _hover={{
                     bg:
                       selected?.name === item.name
-                        ? 'brand.darkBlue'
-                        : 'gray.50',
+                        ? "brand.darkBlue"
+                        : "gray.50",
                   }}
-                  transition='background-color 250ms'
+                  transition="background-color 250ms"
                   value={item}
                   onClick={onOptionClicked(item)}
                 >
-                  <Flex align='center' ml={3}>
+                  <Flex align="center" ml={3}>
                     {/* <Image src={item.flag} w={4} h={4} /> */}
                     <Text
-                      as='span'
-                      role='radio'
-                      fontSize='md'
-                      fontFamily='medium'
+                      as="span"
+                      role="radio"
+                      fontSize="md"
+                      fontFamily="body"
                       fontWeight={500}
-                      d='block'
+                      d="block"
                       isTruncated
                       ml={3}
                     >
@@ -183,12 +182,12 @@ const Select = ({
                   </Flex>
                   {selected?.name === item.name && (
                     <Flex
-                      as='span'
-                      pos='absolute'
+                      as="span"
+                      pos="absolute"
                       top={0}
                       bottom={0}
                       right={0}
-                      align='center'
+                      align="center"
                       pr={4}
                     >
                       <Icon as={Check} />
